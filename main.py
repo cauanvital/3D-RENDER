618d7c62763e06461299f16b749fee5ca6e25107
Cauan Vital <123972992+cauanvital@users.noreply.github.com>
2024-08-19 05:53:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import pygame
+import numpy as np
+from math import *
+from shapes import *
+from render_tools import *
+
+shape = mario
+shape['vertices'] = rotate(shape['vertices'], 90, 0)
+
+
+class RenderApp:
+    def __init__(self):
+        pygame.display.set_caption('3D RENDER')
+        
+        self.RESOLUTION = self.WIDTH, self.HEIGHT = 800, 600
+        self.BG_COLOR = (49, 79, 79)
+        
+        self.VERTICE_COLOR = (255, 0, 0)
+        self.EDGE_COLOR = (255, 255, 255)
+        self.FACE_COLOR = (100, 100, 100)
+        self.SCALE = 6
+        
+        self.screen = pygame.display.set_mode(self.RESOLUTION)
+        self.clock = pygame.time.Clock()
+        
+    def run(self):
+        pygame.init()
+        angle = 0
+        
+        while True:
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        exit()
+                        
+            self.screen.fill(self.BG_COLOR)
+                        
+            rotated_shape = rotate(rotate(shape['vertices'], angle, 1), angle, 0)
+            projected_shape = project_matrix(rotated_shape)
+            rescale_shape = rescale([self.SCALE, self.WIDTH, self.HEIGHT], projected_shape)
+            
+            for v in rescale_shape:
+                pygame.draw.circle(self.screen, self.VERTICE_COLOR, v, 1)
+            draw_edges(self, shape['edges'], rescale_shape)
+                
+                
+            pygame.display.update()
+            
+            angle += 0.01
+
+    
+if __name__ == '__main__':
+    app = RenderApp()
+    app.run()
---
+++ render_tools.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pygame
+from math import *
+
+PROJECTION_MATRIX = np.array([(1, 0, 0),
+                              (0, 1, 0)])
+
+
+def project_matrix(matrix_list):
+    matrix_list = [np.matmul(PROJECTION_MATRIX, matrix) for matrix in matrix_list]
+    
+    return [(float(i[0]), float(i[1])) for i in matrix_list]
+
+def rotate(matrix_list:list[np.array], angle:float = 0, axis:int = None):
+    rotation = 1
+    
+    if axis == 0: # rotate in X axis
+        rotation = np.array([
+            (1, 0, 0),
+            (0, cos(angle), -sin(angle)),
+            (0, sin(angle), cos(angle))
+        ])
+    elif axis == 1: # rotate in Y axis
+        rotation = np.array([
+            (cos(angle), 0, sin(angle)),
+            (0, 1, 0),
+            (-sin(angle), 0, cos(angle))
+        ])
+    elif axis == 2: # rotate in Z axis
+        rotation = np.array([
+            (cos(angle), -sin(angle), 0),
+            (sin(angle), cos(angle), 0),
+            (0, 0, 1)
+        ])
+        
+    matrix_list = [np.matmul(rotation, matrix) for matrix in matrix_list]
+    
+    return matrix_list
+
+def rescale(scales, vertices):
+    rescaled_shape = []
+    
+    for vertex in vertices:
+        x = vertex[0] * scales[0] + scales[1] / 2
+        y = vertex[1] * scales[0] + scales[2] / 2
+        rescaled_shape.append((x,y))
+    
+    return rescaled_shape
+
+def draw_edges(app, edges, vertices):
+    for edge in edges:
+        x1 = edge[0]
+        x2 = edge[1]
+        pygame.draw.line(
+            app.screen,
+            app.EDGE_COLOR,
+            (vertices[x1][0], vertices[x1][1]),
+            (vertices[x2][0], vertices[x2][1]),
+            5
+        )
